25cfec05afeb759d401df6dd65650cc0f6ad51a3
Ernest <ernest20145@gmail.com>
2023-08-25 00:53:26 +0300

    maked navbar responsible

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,11 +2,11 @@ import Link from "next/link"
 
 const Navbar = () => {
     return (
-        <div className="flex bg-500 w-full h-24 items-center  bg-slate-200 flex-inline align-middle rounded-lg">
-            <div className="pr-24 ml-10">
+        <div className="flex  w-full bg-500  h-24 items-center  bg-slate-200  align-middle rounded-lg">
+            <div className=" grow  ml-10">
                 <p className="text-green">HULU</p>
             </div>
-
+<div className=" hidden lg:contents">
         <div className="w-full">
             <ul className="flex flex-row gap-10 lg:gap-20">
                 <li><Link href="/TV">TV Show</Link></li>
@@ -16,9 +16,17 @@ const Navbar = () => {
             </ul>
         </div>
         <div className="mx-2 md:mx-40">
+        
             <span>LOGIN</span>
         </div>
-
+        </div>
+        <div className=" mx-5 lg:hidden">
+        <button  type="button" className=" text-black  ">
+          <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+          </svg>
+        </button>
+      </div>
         </div>
     )
 }
