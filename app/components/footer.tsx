0d45f0894755dda86c04724fd11e7a292cd53a7f
Ernest <ernest20145@gmail.com>
2023-08-24 23:46:34 +0300

    first commit

---
+++ app/components/footer.tsx
@@ -0,0 +1,17 @@
+
+export default function footer() {
+
+
+
+    return (
+<>
+<section>
+    <div className="w-full h-40 bg-slate-600">
+    <ul>
+        <li className="text-red-700">some text</li>
+    </ul>
+    </div>
+</section>
+</>
+     )
+}
