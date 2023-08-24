da76f671bd858a5df3c227b456a8ca9f5b58a1df
demetrovichz <pugehok322@gmail.com>
2023-08-25 00:34:47 +0300

    add roboto

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,22 +3,24 @@ import Link from "next/link"
 const Navbar = () => {
     return (
         <div className="flex bg-500 w-full h-24 items-center  bg-slate-200 flex-inline align-middle rounded-lg">
-            <div className="pr-24 ml-10">
+            <div className="pr-24 ml-10 md:ml-10 sm:ml-2">
                 <p className="text-green">HULU</p>
             </div>
 
         <div className="w-full">
+            <nav>
             <ul className="flex flex-row gap-10 lg:gap-20">
                 <li><Link href="/TV">TV Show</Link></li>
                 <li><Link href="/Movies">Movies</Link></li>
                 <li><Link href="/Originals">Original</Link></li>
-                <li><Link href="/Originals">Kids</Link></li>
+                <li><Link href="/kids">Kids</Link></li>
             </ul>
+            </nav>
+          
         </div>
         <div className="mx-2 md:mx-40">
             <span>LOGIN</span>
         </div>
-
         </div>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
 import MainF from "./components/footer"
 import { Navbar } from './components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets:['cyrillic'], weight:['100','300', '400', '500', '700', '900'], })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <header className='w-full h-48'>
         <Navbar/>
           </header>
